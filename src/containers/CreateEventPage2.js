7f32ffd0efeffeeeeabc9a96566194e6d9bcf059
Kevin Hury <kevinh306@gmail.com>
2016-12-25 15:06:14 +0200

    added initial styles to second page of event creation pipeline

--- src/containers/CreateEventPage2.js
+++ src/containers/CreateEventPage2.js
@@ -1,19 +1,51 @@
+// @flow
+
 import React, { Component, PropTypes } from 'react'
 import {
   View,
   TextInput,
 	StyleSheet
 } from 'react-native'
+import { Button } from 'react-native-elements'
 import { connect } from 'react-redux'
 import { Actions } from 'react-native-router-flux'
 import LinearGradient from 'react-native-linear-gradient'
 import CardView from '../components/CardView'
 import EventCreatePanel from '../components/EventCreatePanel'
 import { FormButton } from '../components/Common'
+import Dialog from '../components/Dialogs/Dialog'
+
+function generateTimeFormat(): Array<string> {
+	const interval = 15
+	return Array.from({
+		length: 24 * 60 / interval
+	}, (v, i) => {
+		let h = Math.floor(i * interval / 60)
+		let m = i * interval - h * 60
+		if (h < 10)
+			h = '0' + h
+		if (m < 10)
+			m = '0' + m
+		return h + ':' + m
+	})
+}
+
 
 class CreateEventPage2 extends Component {
 	static propTypes = {
 
+	}
+
+	setDaysOnClick() {
+		this.refs.numDaysDialog.modal().open()
+	}
+
+	setTimeOnCLick() {
+		this.refs.timeDialog.modal().open()
+	}
+
+	setDeadlineOnClick() {
+		this.refs.deadlineDialog.modal().open()
 	}
 
 	render() {
@@ -27,11 +59,55 @@ class CreateEventPage2 extends Component {
 					<View>
 						<FormButton
 							text='Event length (days)'
-							onPress={() => console.log('press')}
-							style={styles.lengthButton}
+							onPress={() => this.setDaysOnClick()}
+							style={styles.button}
+						/>
+						<FormButton
+							text='Start time'
+							onPress={() => this.setTimeOnCLick()}
+							style={styles.button}
+						/>
+						<FormButton
+							text='RSVP deadline'
+							onPress={() => this.setDeadlineOnClick()}
+							style={styles.button}
+						/>
+					</View>
+					<View style={styles.buttonContainer}>
+						<Button
+							large
+							raised
+							onPress={() => Actions.createEventPage3()}
+							title='Next'
+							backgroundColor='#01a836'
+							buttonStyle={styles.nextButton}
 						/>
 					</View>
 				</CardView>
+				<Dialog
+          ref={'numDaysDialog'}
+          title='How many days?'
+          type={{ name:'picker', options: [ ...Array(10).keys()].map(x => x + 1) }}
+          buttonText='SET'
+          modalStyle={{ height: 280 }}
+          buttonCallback={() => console.log('set')}
+        />
+				<Dialog
+					ref={'timeDialog'}
+					title='What time your event will start?'
+					type={{ name:'picker', options: generateTimeFormat() }}
+					buttonText='SET'
+					modalStyle={{ height: 280 }}
+					buttonCallback={() => console.log('set')}
+				/>
+				<Dialog
+					ref={'deadlineDialog'}
+					title='Invites ends in?'
+					type={{ name:'picker', options: [1, 2, 3, 7, 12, 24, 48, 72].map(x => `${x} Hours`) }}
+					buttonText='SET'
+					modalStyle={{ height: 280 }}
+					buttonCallback={() => console.log('set')}
+				/>
       </LinearGradient>
 		)
 	}
@@ -48,12 +124,20 @@ const styles = StyleSheet.create({
 		margin: 5,
 		marginBottom: 15,
 	},
-	lengthButton: {
+	button: {
 		height: 40,
 		marginLeft: 5,
 		marginRight: 5,
 		margin: 8,
-	}
+	},
+	buttonContainer: {
+		flex: 2,
+		justifyContent: 'flex-end',
+	},
+	nextButton: {
+		height: 50,
+		borderRadius: 15,
+	},
 })
 
 export default connect(null, {})(CreateEventPage2)
