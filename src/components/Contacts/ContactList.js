5c2b38afb83f0d0d0d2975734b6baec948dd87f9
Kevin Hury <kevinh306@gmail.com>
2016-12-25 15:05:30 +0200

    list fix

--- src/components/Contacts/ContactList.js
+++ src/components/Contacts/ContactList.js
@@ -9,81 +9,81 @@ import SectionHeader from './SectionHeader'
 import Header from './Header'
 
 class ContactList extends Component {
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-    selectedList: PropTypes.array.isRequired,
-    onValueChange: PropTypes.func.isRequired,
-  }
+	static propTypes = {
+		contacts: PropTypes.array.isRequired,
+		selectedList: PropTypes.array.isRequired,
+		onValueChange: PropTypes.func.isRequired,
+	}
 
-  componentWillMount() {
-    this.createDataSource(this.props)
-  }
+	componentWillMount() {
+		this.createDataSource(this.props)
+	}
 
-  componentWillReceiveProps(nextProps) {
-    this.createDataSource(nextProps)
-  }
+	componentWillReceiveProps(nextProps: any) {
+		this.createDataSource(nextProps)
+	}
 
-  createDataSource({ contacts }) {
-    const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId]
-    const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`]
+	createDataSource({ contacts }) {
+		const getSectionData = (dataBlob, sectionId) => dataBlob[sectionId]
+		const getRowData = (dataBlob, sectionId, rowId) => dataBlob[`${rowId}`]
 
-    const ds = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2,
-      sectionHeaderHasChanged : (s1, s2) => s1 !== s2,
-      getSectionData,
-      getRowData,
-    })
+		const ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2,
+			sectionHeaderHasChanged : (s1, s2) => s1 !== s2,
+			getSectionData,
+			getRowData,
+		})
 
-    const { dataBlob, sectionIds, rowIds } = this.formatData(contacts)
-    this.dataSource = ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds)
-  }
+		const { dataBlob, sectionIds, rowIds } = this.formatData(contacts)
+		this.dataSource = ds.cloneWithRowsAndSections(dataBlob, sectionIds, rowIds)
+	}
 
-  formatData(data) {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+	formatData(data: any) {
+		const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 
-    const dataBlob = {}
-    const sectionIds = []
-    const rowIds = []
+		const dataBlob = {}
+		const sectionIds = []
+		const rowIds = []
 
-    for (let sectionId = 0; sectionId < alphabet.length; sectionId++) {
-      const currentChar = alphabet[sectionId]
-      const users = data.filter((user) => user.name.toUpperCase().indexOf(currentChar) === 0)
-      if (users.length > 0) {
-        sectionIds.push(sectionId)
-        dataBlob[sectionId] = { character: currentChar }
+		for (let sectionId = 0; sectionId < alphabet.length; sectionId++) {
+			const currentChar = alphabet[sectionId]
+			const users = data.filter((user) => user.name.toUpperCase().indexOf(currentChar) === 0)
+			if (users.length > 0) {
+				sectionIds.push(sectionId)
+				dataBlob[sectionId] = { character: currentChar }
 
-        rowIds.push([])
+				rowIds.push([])
 
-        for (let i = 0; i < users.length; i++) {
-          const rowId = `${sectionId}:${i}`
-          rowIds[rowIds.length - 1].push(rowId)
-          dataBlob[rowId] = users[i]
-        }
-      }
-    }
+				for (let i = 0; i < users.length; i++) {
+					const rowId = `${sectionId}:${i}`
+					rowIds[rowIds.length - 1].push(rowId)
+					dataBlob[rowId] = users[i]
+				}
+			}
+		}
 
-    return { dataBlob, sectionIds, rowIds }
-  }
+		return { dataBlob, sectionIds, rowIds }
+	}
 
-  renderRow(contact) {
-    const selected = this.props.selectedList.includes(contact.recordId)
-    return (
+	renderRow(contact: any) {
+		const selected = this.props.selectedList.includes(contact.recordId)
+		return (
       <ContactCell
         contact={contact}
         selected={selected}
         onValueChange={value => this.props.onValueChange(value, contact)}
       />
-    )
-  }
+		)
+	}
 
-  renderSeparator(sectionId, rowId) {
-    return (
+	renderSeparator(sectionId, rowId) {
+		return (
       <View key={rowId} style={styles.separator} />
-    )
-  }
+		)
+	}
 
-  render() {
-    return (
+	render() {
+		return (
       <ListView
         enableEmptySections
         dataSource={this.dataSource}
@@ -93,19 +93,19 @@ class ContactList extends Component {
         renderHeader={() => <Header />}
         renderSectionHeader={(sectionData) => <SectionHeader { ...sectionData} />}
       />
-    )
-  }
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
+	container: {
 
-  },
-  separator: {
-    flex: 1,
-    height: StyleSheet.hairlineWidth,
-    backgroundColor: '#8E8E8E',
-  }
+	},
+	separator: {
+		flex: 1,
+		height: StyleSheet.hairlineWidth,
+		backgroundColor: '#8E8E8E',
+	}
 })
 
 export default ContactList
