9cfc905856785b2b515e3da46a04923835d86af7
Kevin Hury <kevinh306@gmail.com>
2017-03-13 01:31:39 +0200

    Removed PhoneLogin pages

--- src/Router.js
+++ src/Router.js
@@ -6,23 +6,15 @@ import { Scene, Router, Actions } from 'react-native-router-flux'
 import I18n from 'react-native-i18n'
 
 // Screens
-import PhoneLoginPage from './containers/PhoneLoginPage'
-import PhoneConfirmPage from './containers/PhoneConfirmPage'
 import LobbyPage from './containers/LobbyPage'
 import CreateEventPage from './containers/CreateEventPage'
 import CreateEventPage2 from './containers/CreateEventPage2'
 import CreateEventPage3 from './containers/CreateEventPage3'
 import EventInfoPage from './containers/EventInfoPage'
 
-
-// TODO: change padding top to Navigator.NavigationBar.Styles.General.TotalNavHeight
 const RouterComponent = () => (
 	<View style={styles.background}>
 		<Router sceneStyle={styles.scene} navigationBarStyle={styles.navbar} titleStyle={styles.title} leftButtonIconStyle={styles.leftButton}>
-			<Scene key='auth'>
-				<Scene key='phoneLoginPage' component={PhoneLoginPage} title={I18n.t('navigation.phoneLoginTitle')} />
-				<Scene key='phoneConfirmPage' component={PhoneConfirmPage} title={I18n.t('navigation.phoneConfirmTitle')} />
-			</Scene>
 			<Scene key='main' initial>
 				<Scene key='lobbyPage' component={LobbyPage} title={I18n.t('navigation.eventsTitle')} />
 				<Scene key='eventInfoPage' component={EventInfoPage} title={I18n.t('navigation.eventTitle')} />
@@ -30,7 +22,6 @@ const RouterComponent = () => (
 					<Scene key='createEventPage' component={CreateEventPage} title={I18n.t('navigation.createEventTitle')} leftTitle={I18n.t('cancel')} onLeft={() => Actions.pop()} />
 					<Scene key='createEventPage2' component={CreateEventPage2} title={I18n.t('navigation.createEventTitle')} />
 					<Scene key='createEventPage3' component={CreateEventPage3} title={I18n.t('navigation.createEventTitle')} />
-					<Scene key='eventInfoPage' component={EventInfoPage} title={I18n.t('navigation.eventTitle')} />
 				</Scene>
 			</Scene>
 		</Router>
